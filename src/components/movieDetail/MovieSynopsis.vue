4ccbdc71766908d623b1c2d9fda6279484292ea4
Kyuboem Han <smarthkb98@gmail.com>
2019-07-25 05:21:24 +0900

    review 건들였닷

--- src/components/movieDetail/MovieSynopsis.vue
+++ src/components/movieDetail/MovieSynopsis.vue
@@ -6,7 +6,15 @@
             {{movieDetail.synopsis_content}}
         </div>
 
-        <div>
+        <div class="movie-trailer-title">
+            < 예고편 >
+        </div>
+        <div class="movie_trailer_thumb">
+            <img v-on:click="openVideo(movieDetail)" class="button play"/>
+            <img v-on:click="openVideo(movieDetail)" class="movie-trailer-thumbnail" :src="movieDetail.trailer_thumbnail"/>
+
+           
+            
         </div>
     </div>
    
@@ -16,12 +24,46 @@
 <script>
 export default {
     name: 'MovieSynopsis',
-    props: ['movieDetail']
+    props: ['movieDetail'],
+    methods: {
+        openVideo(movieDetail){
+            window.open('about:blank').location.href = movieDetail.trailer_url
+        }
+    }
 }
 </script>
 
 
 <style>
+    .movie_trailer_thumb {
+        cursor: pointer;
+    }
+
+    .button.play {
+        box-sizing: border-box;
+        width: 74px;
+        height: 74px;
+        border-style: solid;
+        border-width: 37px 0px 37px 74px;
+        border-color: transparent transparent transparent #202020;
+        position: absolute;
+        margin-top: 7rem;
+        margin-left: 11.5rem;
+        z-index : 999;
+    }
+
+    .movie-trailer-thumbnail{
+        z-index: 0;
+        margin-top: 2rem;
+        opacity: 0.5;
+    }
+
+    .movie-trailer-title{
+        margin-top: 4rem;
+        font-weight: bold;
+        font-size: 1.25rem;
+    }
+
     .movie-synopsis-title{
         margin: 0.7rem 0rem 1rem 0rem;
         font-weight: bold;
